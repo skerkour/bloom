b2777dede2d709366a5c1b7d805afc95eef498fa
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-10 21:31:41 +0000

    bloom/analytics: implement repository.create_visitor

--- bloom/analytics/src/repository/create_visitor.rs
+++ bloom/analytics/src/repository/create_visitor.rs
@@ -5,6 +5,24 @@ use stdx::{log::error, sqlx};
 
 impl Repository {
     pub async fn create_visitor<'c, C: Queryer<'c>>(&self, db: C, visitor: &Visitor) -> Result<(), Error> {
-        todo!();
+        const QUERY: &str = "INSERT INTO analytics_visitors
+        (id, created_at, updated_at, anonymous_id, namespace_id)
+        VALUES ($1, $2, $3, $4, $5)";
+
+        match sqlx::query(QUERY)
+            .bind(visitor.id)
+            .bind(visitor.created_at)
+            .bind(visitor.updated_at)
+            .bind(visitor.namespace_id)
+            .bind(visitor.anonymous_id)
+            .execute(db)
+            .await
+        {
+            Err(err) => {
+                error!("analytics.create_visitor: Inserting visitor: {}", &err);
+                Err(err.into())
+            }
+            Ok(_) => Ok(()),
+        }
     }
 }
