478b1491bb8448ec3a051bbaa102b303c8e02ee8
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-10 21:29:53 +0000

    bloom/analytics: implement repository.find_visitor_by_anonymous_id

--- bloom/analytics/src/repository/find_visitor_by_anonymous_id.rs
+++ bloom/analytics/src/repository/find_visitor_by_anonymous_id.rs
@@ -1,7 +1,7 @@
 use super::Repository;
 use crate::{entities::Visitor, Error};
 use kernel::db::Queryer;
-use stdx::uuid::Uuid;
+use stdx::{log::error, sqlx, uuid::Uuid};
 
 impl Repository {
     pub async fn find_visitor_by_anonymous_id<'c, C: Queryer<'c>>(
@@ -9,6 +9,19 @@ impl Repository {
         db: C,
         anonymous_id: Uuid,
     ) -> Result<Visitor, Error> {
-        todo!();
+        const QUERY: &str = "SELECT * FROM analytics_visitors WHERE anonymous_id = $1";
+
+        match sqlx::query_as::<_, Visitor>(QUERY)
+            .bind(anonymous_id)
+            .fetch_optional(db)
+            .await
+        {
+            Err(err) => {
+                error!("analytics.find_visitor_by_anonymous_id: finding visitor: {}", &err);
+                Err(err.into())
+            }
+            Ok(None) => Err(Error::VisitorNotFound),
+            Ok(Some(res)) => Ok(res),
+        }
     }
 }
