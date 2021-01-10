4653115df51cc0008fef5eef08ba13aa7087556a
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-10 21:33:53 +0000

    bloom/analytics: implement service.device_type_from_screen_size

--- bloom/analytics/src/service/utils.rs
+++ bloom/analytics/src/service/utils.rs
@@ -2,6 +2,14 @@ use crate::{entities::DeviceType, Service};
 
 impl Service {
     pub fn device_type_from_screen_size(&self, screen_width: i64, screen_height: i64) -> DeviceType {
-        todo!();
+        if screen_width < 600 {
+            return DeviceType::Phone;
+        } else if screen_width > 600 && screen_width <= 960 {
+            return DeviceType::Tablet;
+        } else if screen_width > 960 && screen_width <= 2080 {
+            return DeviceType::Desktop;
+        } else {
+            return DeviceType::Other;
+        }
     }
 }
