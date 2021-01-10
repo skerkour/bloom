0043bce12ba36b0fa473e2545046a49730978e32
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-10 21:25:31 +0000

    bloom/analytics: implement repository.find_events

--- bloom/analytics/src/repository/find_events.rs
+++ bloom/analytics/src/repository/find_events.rs
@@ -1,10 +1,25 @@
 use super::Repository;
 use crate::{entities::Event, Error};
 use kernel::db::Queryer;
-use stdx::uuid::Uuid;
+use stdx::{log::error, sqlx, uuid::Uuid};
 
 impl Repository {
     pub async fn find_events<'c, C: Queryer<'c>>(&self, db: C, namespace_id: Uuid) -> Result<Vec<Event>, Error> {
-        todo!();
+        const QUERY: &str = "SELECT event_name, COUNT (id) AS views, COUNT (DISTINCT visitor_id) AS visitors
+		FROM analytics_track_events
+		WHERE namespace_id = $1 AND timestamp > (CURRENT_DATE - INTERVAL '30 days')
+		GROUP BY event_name";
+
+        match sqlx::query_as::<_, Event>(QUERY)
+            .bind(namespace_id)
+            .fetch_all(db)
+            .await
+        {
+            Err(err) => {
+                error!("analytics.find_events: finding events: {}", &err);
+                Err(err.into())
+            }
+            Ok(res) => Ok(res),
+        }
     }
 }
