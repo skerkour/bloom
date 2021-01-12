2d7c8397008461eb5ddc87c1d2b7653e3253f049
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-12 11:49:23 +0000

    bloom/files: implement repository.create_file

--- bloom/files/src/repository/create_file.rs
+++ bloom/files/src/repository/create_file.rs
@@ -1,9 +1,33 @@
 use super::Repository;
 use crate::{entities::File, Error};
 use kernel::db::Queryer;
+use stdx::{log::error, sqlx};
 
 impl Repository {
     pub async fn create_file<'c, C: Queryer<'c>>(&self, db: C, file: &File) -> Result<(), Error> {
-        todo!();
+        const QUERY: &str = "INSERT INTO files
+        (id, created_at, updated_at, name, size, type, explicitly_trashed, trashed_at, namespace_id, parent_id)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)";
+
+        match sqlx::query(QUERY)
+            .bind(file.id)
+            .bind(file.created_at)
+            .bind(file.updated_at)
+            .bind(&file.name)
+            .bind(file.size)
+            .bind(&file.r#type)
+            .bind(file.explicitly_trashed)
+            .bind(file.trashed_at)
+            .bind(file.namespace_id)
+            .bind(file.parent_id)
+            .execute(db)
+            .await
+        {
+            Err(err) => {
+                error!("files.create_file: Inserting file: {}", &err);
+                Err(err.into())
+            }
+            Ok(_) => Ok(()),
+        }
     }
 }
