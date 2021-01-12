1a0f9a599f593321da51305d40958fdfe91f1f5e
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-12 12:13:15 +0000

    bloom/files: implement repository.find_all_trashed_files

--- bloom/files/src/repository/find_all_trashed_files.rs
+++ bloom/files/src/repository/find_all_trashed_files.rs
@@ -1,7 +1,7 @@
 use super::Repository;
 use crate::{entities::File, Error};
 use kernel::db::Queryer;
-use stdx::uuid::Uuid;
+use stdx::{chrono::Utc, log::error, sqlx, uuid::Uuid};
 
 impl Repository {
     pub async fn find_all_trashed_files<'c, C: Queryer<'c>>(
@@ -9,6 +9,23 @@ impl Repository {
         db: C,
         namespace_id: Uuid,
     ) -> Result<Vec<File>, Error> {
-        todo!();
+        const QUERY: &str = "SELECT * FROM files
+		WHERE namespace_id = $1 AND trashed_at IS NOT NULL
+		ORDER BY id DESC";
+
+        let now = Utc::now();
+
+        match sqlx::query_as::<_, File>(QUERY)
+            .bind(now)
+            .bind(namespace_id)
+            .fetch_all(db)
+            .await
+        {
+            Err(err) => {
+                error!("files.find_all_trashed_files: Finding file: {}", &err);
+                Err(err.into())
+            }
+            Ok(files) => Ok(files),
+        }
     }
 }
