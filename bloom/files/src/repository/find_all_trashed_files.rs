ca669570f7576b8f2b4036066e9e6ed39a0fa07d
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-12 12:17:07 +0000

    bloom/files: implement repository.find_children_recursively

--- bloom/files/src/repository/find_all_trashed_files.rs
+++ bloom/files/src/repository/find_all_trashed_files.rs
@@ -1,7 +1,7 @@
 use super::Repository;
 use crate::{entities::File, Error};
 use kernel::db::Queryer;
-use stdx::{chrono::Utc, log::error, sqlx, uuid::Uuid};
+use stdx::{log::error, sqlx, uuid::Uuid};
 
 impl Repository {
     pub async fn find_all_trashed_files<'c, C: Queryer<'c>>(
@@ -11,18 +11,15 @@ impl Repository {
     ) -> Result<Vec<File>, Error> {
         const QUERY: &str = "SELECT * FROM files
 		WHERE namespace_id = $1 AND trashed_at IS NOT NULL
-		ORDER BY id DESC";
-
-        let now = Utc::now();
+        ORDER BY id DESC";
 
         match sqlx::query_as::<_, File>(QUERY)
-            .bind(now)
             .bind(namespace_id)
             .fetch_all(db)
             .await
         {
             Err(err) => {
-                error!("files.find_all_trashed_files: Finding file: {}", &err);
+                error!("files.find_all_trashed_files: Finding files: {}", &err);
                 Err(err.into())
             }
             Ok(files) => Ok(files),
--- bloom/files/src/repository/find_children_recursively.rs
+++ bloom/files/src/repository/find_children_recursively.rs
@@ -1,7 +1,7 @@
 use super::Repository;
 use crate::{entities::File, Error};
 use kernel::db::Queryer;
-use stdx::uuid::Uuid;
+use stdx::{log::error, sqlx, uuid::Uuid};
 
 impl Repository {
     pub async fn find_children_recursively<'c, C: Queryer<'c>>(
@@ -9,6 +9,32 @@ impl Repository {
         db: C,
         file_id: Uuid,
     ) -> Result<Vec<File>, Error> {
-        todo!();
+        const QUERY: &str = "	WITH RECURSIVE tree AS (
+            SELECT id, created_at, updated_at, name, size, type, explicitly_trashed, trashed_at, namespace_id,
+                parent_id, ARRAY[]::UUID[] AS ancestors
+            FROM files WHERE parent_id IS NULL
+
+            UNION ALL
+
+            SELECT files.id, files.created_at, files.updated_at,
+            files.name, files.size, files.type,
+            files.explicitly_trashed, files.trashed_at, files.namespace_id,
+            files.parent_id, tree.ancestors || files.parent_id
+            FROM files, tree
+            WHERE files.parent_id = tree.id
+          ) SELECT id, created_at, updated_at, name, size, type, explicitly_trashed, trashed_at, namespace_id, parent_id
+          FROM tree WHERE $1 = ANY(tree.ancestors);";
+
+        match sqlx::query_as::<_, File>(QUERY)
+            .bind(file_id)
+            .fetch_all(db)
+            .await
+        {
+            Err(err) => {
+                error!("files.find_children_recursively: Finding files: {}", &err);
+                Err(err.into())
+            }
+            Ok(files) => Ok(files),
+        }
     }
 }
