17051fcaecee9f2cff91cc32fe06c2dea0f64d3c
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-12 12:21:25 +0000

    bloom/files: implement repository.find_files_by_ids

--- bloom/files/src/repository/find_files_by_ids.rs
+++ bloom/files/src/repository/find_files_by_ids.rs
@@ -1,10 +1,23 @@
 use super::Repository;
 use crate::{entities::File, Error};
 use kernel::db::Queryer;
-use stdx::uuid::Uuid;
+use stdx::{log::error, sqlx, uuid::Uuid};
 
 impl Repository {
     pub async fn find_files_by_ids<'c, C: Queryer<'c>>(&self, db: C, file_ids: &[Uuid]) -> Result<Vec<File>, Error> {
-        todo!();
+        const QUERY: &str = "SELECT * FROM files
+            WHERE id = ANY($1)";
+
+        match sqlx::query_as::<_, File>(QUERY)
+            .bind(file_ids)
+            .fetch_all(db)
+            .await
+        {
+            Err(err) => {
+                error!("files.find_files_by_ids: Finding files: {}", &err);
+                Err(err.into())
+            }
+            Ok(files) => Ok(files),
+        }
     }
 }
