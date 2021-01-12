e368d165d9df58a625ff0963e9d0e8dab3c4b7a6
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-12 12:22:35 +0000

    bloom/files: implement repository.find_root_file_for_namespace

--- bloom/files/src/repository/find_root_file_for_namespace.rs
+++ bloom/files/src/repository/find_root_file_for_namespace.rs
@@ -1,5 +1,5 @@
 use super::Repository;
-use crate::{entities, Error};
+use crate::{Error, consts, entities};
 use kernel::db::Queryer;
 use stdx::sqlx;
 use stdx::{log::error, uuid::Uuid};
@@ -10,6 +10,21 @@ impl Repository {
         db: C,
         namespace_id: Uuid,
     ) -> Result<entities::File, Error> {
-        todo!();
+        const QUERY: &str = "SELECT * FROM files
+            WHERE namespace_id = $1 AND name = $2";
+
+    match sqlx::query_as::<_, entities::File>(QUERY)
+        .bind(namespace_id)
+        .bind(consts::ROOT_FILE_NAME)
+        .fetch_optional(db)
+        .await
+    {
+        Err(err) => {
+            error!("files.find_root_file_for_namespace: finding file: {}", &err);
+            Err(err.into())
+        }
+        Ok(None) => Err(Error::FileNotFound),
+        Ok(Some(res)) => Ok(res),
+    }
     }
 }
