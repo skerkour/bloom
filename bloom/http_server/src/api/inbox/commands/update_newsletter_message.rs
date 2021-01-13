f4ae4e1d67365dbcfa3251bfcdb639df81b668bd
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-13 11:28:33 +0000

    bloom/http_server: implement inbox.update_newsletter_message

--- bloom/http_server/src/api/inbox/commands/update_newsletter_message.rs
+++ bloom/http_server/src/api/inbox/commands/update_newsletter_message.rs
@@ -3,14 +3,22 @@ use crate::{
     ServerContext,
 };
 use actix_web::web;
+use inbox::service::UpdateNewsletterMessageInput;
 use kernel::{http::api, Actor};
 use std::sync::Arc;
 use web::Json;
 
 pub async fn update_newsletter_message(
-    _ctx: web::Data<Arc<ServerContext>>,
-    _input: Json<input::UpdateNewsletterMessage>,
-    _actor: Actor,
+    ctx: web::Data<Arc<ServerContext>>,
+    input: Json<input::UpdateNewsletterMessage>,
+    actor: Actor,
 ) -> Result<api::Response<model::NewsletterMessage>, kernel::Error> {
-    todo!();
+    let input = input.into_inner();
+    let service_input = UpdateNewsletterMessageInput {};
+    let message = ctx
+        .inbox_service
+        .update_newsletter_message(actor, service_input)
+        .await?;
+
+    Ok(api::Response::ok(message.into()))
 }
