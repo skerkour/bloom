3b1973dedd440036e54444de500239d773e9e7cb
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-13 11:22:23 +0000

    bloom/http_server: implement inbox.delete_newsletter_message

--- bloom/http_server/src/api/inbox/commands/delete_newsletter_message.rs
+++ bloom/http_server/src/api/inbox/commands/delete_newsletter_message.rs
@@ -3,14 +3,21 @@ use crate::{
     ServerContext,
 };
 use actix_web::web;
+use inbox::service::DeleteNewsletterMessageInput;
 use kernel::{http::api, Actor};
 use std::sync::Arc;
 use web::Json;
 
 pub async fn delete_newsletter_message(
-    _ctx: web::Data<Arc<ServerContext>>,
-    _input: Json<input::DeleteNewsletterMessage>,
-    _actor: Actor,
+    ctx: web::Data<Arc<ServerContext>>,
+    input: Json<input::DeleteNewsletterMessage>,
+    actor: Actor,
 ) -> Result<api::Response<Success>, kernel::Error> {
-    todo!();
+    let input = input.into_inner();
+    let service_input = DeleteNewsletterMessageInput {};
+    ctx.inbox_service
+        .delete_newsletter_message(actor, service_input)
+        .await?;
+
+    Ok(api::Response::ok(true.into()))
 }
