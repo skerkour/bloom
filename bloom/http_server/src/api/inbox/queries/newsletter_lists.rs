2b0d6c726c61785da029805e62f5ce55c79d250f
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-13 11:31:57 +0000

    bloom/http_server: implement inbox.newsletter_lists

--- bloom/http_server/src/api/inbox/queries/newsletter_lists.rs
+++ bloom/http_server/src/api/inbox/queries/newsletter_lists.rs
@@ -3,14 +3,19 @@ use crate::{
     ServerContext,
 };
 use actix_web::web;
+use inbox::service::FindNewsletterListsInput;
 use kernel::{http::api, Actor};
 use std::sync::Arc;
 use web::Json;
 
 pub async fn newsletter_lists(
-    _ctx: web::Data<Arc<ServerContext>>,
-    _input: Json<input::GetNewsletterLists>,
-    _actor: Actor,
+    ctx: web::Data<Arc<ServerContext>>,
+    input: Json<input::GetNewsletterLists>,
+    actor: Actor,
 ) -> Result<api::Response<Vec<model::NewsletterList>>, kernel::Error> {
-    todo!();
+    let input = input.into_inner();
+    let service_input = FindNewsletterListsInput {};
+    let lists = ctx.inbox_service.find_newsletter_lists(actor, service_input).await?;
+
+    Ok(api::Response::ok(lists.into_iter().map(Into::into).collect()))
 }
