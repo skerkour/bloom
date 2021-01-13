5c54754cbdc17a77ccc861a1258f6f620ec3c3eb
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-13 11:37:14 +0000

    bloom/inbox: entities.Contact

--- bloom/inbox/src/entities.rs
+++ bloom/inbox/src/entities.rs
@@ -16,6 +16,28 @@ pub struct Contact {
     pub id: Uuid,
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
+
+    pub name: String,
+    pub birthday: Option<DateTime<Utc>>,
+    pub email: String,
+    pub pgp_key: String,
+    pub phone: String,
+    pub address: String,
+    pub website: String,
+    pub twitter: String,
+    pub instagram: String,
+    pub facebook: String,
+    pub linkedin: String,
+    pub skype: String,
+    pub telegram: String,
+    pub bloom: String,
+    pub notes: String,
+    pub country: String,
+    pub country_code: String,
+    pub plan: String,
+    pub user_id: String,
+
+    pub namespace_id: Uuid,
 }
 
 #[derive(sqlx::FromRow, Debug, Clone)]
