17c356f6c5c6db5110e4e97673a7350a0ce9d8d3
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-13 11:30:35 +0000

    bloom/http_server: implement inbox.newsletter_message

--- bloom/http_server/src/api/inbox/queries/newsletter_message.rs
+++ bloom/http_server/src/api/inbox/queries/newsletter_message.rs
@@ -3,14 +3,19 @@ use crate::{
     ServerContext,
 };
 use actix_web::web;
+use inbox::service::FindNewsletterMessageInput;
 use kernel::{http::api, Actor};
 use std::sync::Arc;
 use web::Json;
 
 pub async fn newsletter_message(
-    _ctx: web::Data<Arc<ServerContext>>,
-    _input: Json<input::GetNewsletterMessage>,
-    _actor: Actor,
+    ctx: web::Data<Arc<ServerContext>>,
+    input: Json<input::GetNewsletterMessage>,
+    actor: Actor,
 ) -> Result<api::Response<model::NewsletterMessage>, kernel::Error> {
-    todo!();
+    let input = input.into_inner();
+    let service_input = FindNewsletterMessageInput {};
+    let message = ctx.inbox_service.find_newsletter_message(actor, service_input).await?;
+
+    Ok(api::Response::ok(message.into()))
 }
