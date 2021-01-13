88ee11808db7f687d2ad932a9d05487fc0b57bbc
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-13 11:16:30 +0000

    bloom/http_server: implement inbox.create_contact

--- bloom/http_server/src/api/inbox/commands/create_contact.rs
+++ bloom/http_server/src/api/inbox/commands/create_contact.rs
@@ -3,14 +3,20 @@ use crate::{
     ServerContext,
 };
 use actix_web::web;
+use inbox::service::CreateContactInput;
 use kernel::{http::api, Actor};
 use std::sync::Arc;
 use web::Json;
 
 pub async fn create_contact(
-    _ctx: web::Data<Arc<ServerContext>>,
-    _input: Json<input::CreateContact>,
-    _actor: Actor,
+    ctx: web::Data<Arc<ServerContext>>,
+    input: Json<input::CreateContact>,
+    actor: Actor,
 ) -> Result<api::Response<model::Contact>, kernel::Error> {
-    todo!();
+    let input = input.into_inner();
+    let service_input = CreateContactInput {
+    };
+    let contact = ctx.inbox_service.create_contact(actor, service_input).await?;
+
+    Ok(api::Response::ok(contact.into()))
 }
