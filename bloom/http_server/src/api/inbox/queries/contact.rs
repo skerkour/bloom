a55afffc1bf9d1df1159b5d5f7537a12157db07d
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-13 11:29:19 +0000

    bloom/http_server: implement inbox.contact

--- bloom/http_server/src/api/inbox/queries/contact.rs
+++ bloom/http_server/src/api/inbox/queries/contact.rs
@@ -3,14 +3,19 @@ use crate::{
     ServerContext,
 };
 use actix_web::web;
+use inbox::service::FindContactInput;
 use kernel::{http::api, Actor};
 use std::sync::Arc;
 use web::Json;
 
 pub async fn contact(
-    _ctx: web::Data<Arc<ServerContext>>,
-    _input: Json<input::GetContact>,
-    _actor: Actor,
+    ctx: web::Data<Arc<ServerContext>>,
+    input: Json<input::GetContact>,
+    actor: Actor,
 ) -> Result<api::Response<model::Contact>, kernel::Error> {
-    todo!();
+    let input = input.into_inner();
+    let service_input = FindContactInput {};
+    let contact = ctx.inbox_service.find_contact(actor, service_input).await?;
+
+    Ok(api::Response::ok(contact.into()))
 }
