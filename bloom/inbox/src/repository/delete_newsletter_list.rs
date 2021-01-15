5298c9d4e9a435d336c0fffee93f000abaea1cba
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-15 21:56:00 +0000

    bloom/inbox: implement repository.delete_newsletter_list

--- bloom/inbox/src/repository/delete_newsletter_list.rs
+++ bloom/inbox/src/repository/delete_newsletter_list.rs
@@ -6,6 +6,14 @@ use stdx::{log::error, uuid::Uuid};
 
 impl Repository {
     pub async fn delete_newsletter_list<'c, C: Queryer<'c>>(&self, db: C, list_id: Uuid) -> Result<(), Error> {
-        todo!();
+        const QUERY: &str = "DELETE FROM inbox_newsletter_lists WHERE id = $1";
+
+        match sqlx::query(QUERY).bind(list_id).execute(db).await {
+            Err(err) => {
+                error!("inbox.delete_newsletter_list: Deleting list: {}", &err);
+                Err(err.into())
+            }
+            Ok(_) => Ok(()),
+        }
     }
 }
