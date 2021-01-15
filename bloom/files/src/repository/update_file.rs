b2e4800201f54715094d458cb8970d56177d2673
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-15 22:27:28 +0000

    bloom/inbox: implement repository.update_chatbox_preferences

--- bloom/files/src/repository/update_file.rs
+++ bloom/files/src/repository/update_file.rs
@@ -6,8 +6,8 @@ use stdx::{log::error, sqlx};
 impl Repository {
     pub async fn update_file<'c, C: Queryer<'c>>(&self, db: C, file: &File) -> Result<(), Error> {
         const QUERY: &str = "UPDATE files SET
-		updated_at = $1, name = $2, size = $3, type = $4, explicitly_trashed = $5, trashed_at = $6, parent_id = $7, namespace_id = $8
-		WHERE id = $9";
+            updated_at = $1, name = $2, size = $3, type = $4, explicitly_trashed = $5, trashed_at = $6, parent_id = $7, namespace_id = $8
+            WHERE id = $9";
 
         match sqlx::query(QUERY)
             .bind(file.updated_at)
--- bloom/inbox/src/repository/update_chatbox_preferences.rs
+++ bloom/inbox/src/repository/update_chatbox_preferences.rs
@@ -9,6 +9,27 @@ impl Repository {
         db: C,
         preferences: &ChatboxPreferences,
     ) -> Result<(), Error> {
-        todo!();
+        const QUERY: &str = "UPDATE inbox_chatbox_preferences SET
+            updated_at = $1, color = $2, name = $3, avatar_storage_key = $4, show_branding = $5,
+                welcome_message = $6
+            WHERE id = $7";
+
+        match sqlx::query(QUERY)
+            .bind(preferences.updated_at)
+            .bind(&preferences.color)
+            .bind(&preferences.name)
+            .bind(&preferences.avatar_storage_key)
+            .bind(preferences.show_branding)
+            .bind(&preferences.welcome_message)
+            .bind(preferences.id)
+            .execute(db)
+            .await
+        {
+            Err(err) => {
+                error!("files.update_chatbox_preferences: Updating preferences: {}", &err);
+                Err(err.into())
+            }
+            Ok(_) => Ok(()),
+        }
     }
 }
