a154e81ed26b3d637a59cf38384f99606ac8e38b
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-15 22:29:12 +0000

    bloom/inbox: implement repository.update_newsletter_list

--- bloom/inbox/src/repository/update_newsletter_list.rs
+++ bloom/inbox/src/repository/update_newsletter_list.rs
@@ -5,6 +5,23 @@ use stdx::{log::error, sqlx};
 
 impl Repository {
     pub async fn update_newsletter_list<'c, C: Queryer<'c>>(&self, db: C, list: &NewsletterList) -> Result<(), Error> {
-        todo!();
+        const QUERY: &str = "UPDATE inbox_newsletter_lists SET
+            updated_at = $1, name = $2, description = $3
+            WHERE id = $4";
+
+        match sqlx::query(QUERY)
+            .bind(list.updated_at)
+            .bind(&list.name)
+            .bind(&list.description)
+            .bind(list.id)
+            .execute(db)
+            .await
+        {
+            Err(err) => {
+                error!("files.update_newsletter_list: Updating list: {}", &err);
+                Err(err.into())
+            }
+            Ok(_) => Ok(()),
+        }
     }
 }
