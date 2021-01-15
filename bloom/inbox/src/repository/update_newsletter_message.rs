59ff7ac17f2052f7b11fa281ac8c797c5ca2417b
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-15 22:32:28 +0000

    bloom/inbox: implement repository.update_newsletter_message

--- bloom/inbox/src/repository/update_newsletter_message.rs
+++ bloom/inbox/src/repository/update_newsletter_message.rs
@@ -9,6 +9,32 @@ impl Repository {
         db: C,
         message: &NewsletterMessage,
     ) -> Result<(), Error> {
-        todo!();
+        const QUERY: &str = "UPDATE inbox_newsletter_messages SET
+            updated_at = $1, name = $2, subject = $3, body = $4, body_html = $5, status = $6, scheduled_for = $7,
+            last_sent_at = $8, sent_count = $9, error_count = $10, list_id = $11
+            WHERE id = $12";
+
+        match sqlx::query(QUERY)
+            .bind(message.updated_at)
+            .bind(&message.name)
+            .bind(&message.subject)
+            .bind(&message.body)
+            .bind(&message.body_html)
+            .bind(message.status)
+            .bind(message.scheduled_for)
+            .bind(message.last_sent_at)
+            .bind(message.sent_count)
+            .bind(message.error_count)
+            .bind(message.list_id)
+            .bind(message.id)
+            .execute(db)
+            .await
+        {
+            Err(err) => {
+                error!("files.update_newsletter_message: Updating message: {}", &err);
+                Err(err.into())
+            }
+            Ok(_) => Ok(()),
+        }
     }
 }
