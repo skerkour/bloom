aebcfa7222123188a7763b8562617769f21e493b
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-15 16:16:21 +0000

    bloom/inbox: implement service.find_trash

--- bloom/inbox/src/service/find_trash.rs
+++ bloom/inbox/src/service/find_trash.rs
@@ -3,7 +3,18 @@ use crate::{entities::Conversation, Service};
 use kernel::Actor;
 
 impl Service {
-    pub async fn find_trash(&self, _actor: Actor, _input: FindTrashInput) -> Result<Vec<Conversation>, kernel::Error> {
-        todo!();
+    pub async fn find_trash(&self, actor: Actor, input: FindTrashInput) -> Result<Vec<Conversation>, kernel::Error> {
+        // TODO: messages
+        let actor = self.kernel_service.current_user(actor)?;
+
+        self.kernel_service
+            .check_namespace_membership(&self.db, actor.id, input.namespace_id)
+            .await?;
+
+        let conversations = self
+            .repo
+            .find_trashed_conversations(&self.db, input.namespace_id)
+            .await?;
+        Ok(conversations)
     }
 }
