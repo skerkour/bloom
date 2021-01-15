f69e8f3f0d8297a17a6c1481a88ed56b25219ced
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-15 21:55:23 +0000

    bloom/inbox: implement repository.delete_contact

--- bloom/inbox/src/repository/delete_contact.rs
+++ bloom/inbox/src/repository/delete_contact.rs
@@ -5,6 +5,14 @@ use stdx::{log::error, sqlx, uuid::Uuid};
 
 impl Repository {
     pub async fn delete_contact<'c, C: Queryer<'c>>(&self, db: C, contact_id: Uuid) -> Result<(), Error> {
-        todo!();
+        const QUERY: &str = "DELETE FROM inbox_contacts WHERE id = $1";
+
+        match sqlx::query(QUERY).bind(contact_id).execute(db).await {
+            Err(err) => {
+                error!("inbox.delete_contact: Deleting contact: {}", &err);
+                Err(err.into())
+            }
+            Ok(_) => Ok(()),
+        }
     }
 }
