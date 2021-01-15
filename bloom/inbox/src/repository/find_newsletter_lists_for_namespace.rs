04171855ca28216514a2a6842db41f25e4e6fcb7
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-15 22:21:30 +0000

    bloom/inbox: implement repository.find_newsletter_lists_for_namespace

--- bloom/inbox/src/repository/find_newsletter_lists_for_namespace.rs
+++ bloom/inbox/src/repository/find_newsletter_lists_for_namespace.rs
@@ -9,6 +9,19 @@ impl Repository {
         db: C,
         namespace_id: Uuid,
     ) -> Result<Vec<entities::NewsletterList>, Error> {
-        todo!();
+        const QUERY: &str = "SELECT * FROM inbox_newsletter_lists
+            WHERE namespace_id = $1 ORDER BY id";
+
+        match sqlx::query_as::<_, entities::NewsletterList>(QUERY)
+            .bind(namespace_id)
+            .fetch_all(db)
+            .await
+        {
+            Err(err) => {
+                error!("inbox.find_newsletter_lists_for_namespace: Finding lists: {}", &err);
+                Err(err.into())
+            }
+            Ok(lists) => Ok(lists),
+        }
     }
 }
