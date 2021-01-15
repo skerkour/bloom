c1aa6079c702d33632d2971b3513238a9976c175
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-15 22:18:23 +0000

    bloom/inbox: implement repository.find_spam_conversations

--- bloom/inbox/src/repository/find_spam_conversations.rs
+++ bloom/inbox/src/repository/find_spam_conversations.rs
@@ -9,6 +9,21 @@ impl Repository {
         db: C,
         namespace_id: Uuid,
     ) -> Result<Vec<entities::Conversation>, Error> {
-        todo!();
+        const QUERY: &str = "SELECT * FROM inbox_conversations
+            WHERE namespace_id = $1 AND trashed_at IS NULL AND is_spam = $2
+            ORDER BY last_message_at DESC";
+
+        match sqlx::query_as::<_, entities::Conversation>(QUERY)
+            .bind(namespace_id)
+            .bind(true)
+            .fetch_all(db)
+            .await
+        {
+            Err(err) => {
+                error!("inbox.find_spam_conversations: Finding conversations: {}", &err);
+                Err(err.into())
+            }
+            Ok(conversations) => Ok(conversations),
+        }
     }
 }
