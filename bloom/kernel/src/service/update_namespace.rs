fce8ddcce7b230c055dfa787675c69ff82c69e75
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-22 22:23:27 +0000

    bloom/kernel: implement service.update_namespace

--- bloom/kernel/src/service/update_namespace.rs
+++ bloom/kernel/src/service/update_namespace.rs
@@ -2,11 +2,8 @@ use super::Service;
 use crate::{db::Queryer, entities::Namespace};
 
 impl Service {
-    pub async fn update_namespace<'c, C: Queryer<'c>>(
-        &self,
-        _db: C,
-        _namespace: &Namespace,
-    ) -> Result<(), crate::Error> {
-        todo!();
+    pub async fn update_namespace<'c, C: Queryer<'c>>(&self, db: C, namespace: &Namespace) -> Result<(), crate::Error> {
+        self.repo.update_namespace(db, namespace).await?;
+        Ok(())
     }
 }
