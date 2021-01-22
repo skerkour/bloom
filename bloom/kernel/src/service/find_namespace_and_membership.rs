3ed399a7a3e501f35c9973ecd67ca8b8e44a1576
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-22 22:21:37 +0000

    bloom/kernel: implement service.find_namespace_and_membership

--- bloom/kernel/src/service/find_namespace_and_membership.rs
+++ bloom/kernel/src/service/find_namespace_and_membership.rs
@@ -1,17 +1,35 @@
 use super::Service;
 use crate::{
-    db::Queryer,
+    consts::NamespaceType,
+    db::DB,
     entities::{GroupMembership, Namespace},
+    errors::kernel::Error,
 };
 use stdx::uuid::Uuid;
 
 impl Service {
-    pub async fn find_namespace_and_membership<'c, C: Queryer<'c>>(
+    pub async fn find_namespace_and_membership(
         &self,
-        _db: C,
-        _user_id: Uuid,
-        _namespace_id: Uuid,
+        db: &DB,
+        user_id: Uuid,
+        namespace_id: Uuid,
     ) -> Result<(Namespace, Option<GroupMembership>), crate::Error> {
-        todo!();
+        let namespace = self.repo.find_namespace_by_id(db, namespace_id).await?;
+
+        let membership = if namespace.r#type == NamespaceType::Group {
+            Some(match self
+                .repo
+                .find_namespace_group_membership(db, namespace_id, user_id)
+                .await
+            {
+                Ok(membership) => Ok(membership),
+                Err(Error::GroupMemberNotFound) => Err(Error::PermissionDenied),
+                Err(err) => Err(err),
+            }?)
+        } else {
+            None
+        };
+
+        Ok((namespace, membership))
     }
 }
