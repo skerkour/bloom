32d11f53e2db5e205f88916268acbb22f9bf7945
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-22 22:02:42 +0000

    bloom/kernel: implement service.find_group_and_membership

--- bloom/kernel/src/service/find_group_and_membership.rs
+++ bloom/kernel/src/service/find_group_and_membership.rs
@@ -1,17 +1,26 @@
 use super::Service;
 use crate::{
-    db::Queryer,
+    db::DB,
     entities::{Group, GroupMembership},
+    errors::kernel::Error,
 };
 use stdx::uuid::Uuid;
 
 impl Service {
-    pub async fn find_group_and_membership<'c, C: Queryer<'c>>(
+    pub async fn find_group_and_membership(
         &self,
-        _db: C,
-        _user_id: Uuid,
-        _group_id: Uuid,
+        db: &DB,
+        user_id: Uuid,
+        group_id: Uuid,
     ) -> Result<(Group, GroupMembership), crate::Error> {
-        todo!();
+        let membership = match self.repo.find_group_membership(db, group_id, user_id).await {
+            Ok(membership) => Ok(membership),
+            Err(Error::GroupMemberNotFound) => Err(Error::PermissionDenied),
+            Err(err) => Err(err),
+        }?;
+
+        let group = self.repo.find_group_by_id(db, group_id).await?;
+
+        Ok((group, membership))
     }
 }
