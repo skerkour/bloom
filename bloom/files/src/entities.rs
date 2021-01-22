9fc632b0e23269e55521e36b706d153101cd0c28
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-22 19:02:13 +0000

    bloom/files: implement storage_key

--- bloom/files/src/entities.rs
+++ bloom/files/src/entities.rs
@@ -28,6 +28,38 @@ impl File {
     }
 
     pub fn storage_key(&self) -> String {
-        todo!();
+        // TODO: improve
+        let id_str = self.id.to_hyphenated().to_string();
+        format!("/files/{}/{}", &id_str[..4], &id_str)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::File;
+    use stdx::{chrono::Utc, uuid::Uuid};
+
+    #[test]
+    fn file_storage_key() {
+        let id = "c2ae4298-48a2-478b-a9f2-5eef5d9b54cd".parse::<Uuid>().unwrap();
+        let now = Utc::now();
+        let expected_storage_key = "/files/c2ae/c2ae4298-48a2-478b-a9f2-5eef5d9b54cd".to_string();
+
+        let file = File {
+            id,
+            created_at: now,
+            updated_at: now,
+
+            name: String::new(),
+            size: 0,
+            r#type: String::new(),
+            explicitly_trashed: false,
+            trashed_at: None,
+
+            namespace_id: None,
+            parent_id: None,
+        };
+
+        assert_eq!(file.storage_key(), expected_storage_key);
     }
 }
