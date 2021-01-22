9dfbb94ff4a2b96df4c4a6e6b1f588d4d5124d0c
Sylvain Kerkour <sylvain@kerkour.fr>
2021-01-22 20:12:44 +0000

    bloom/files: fix file.storage_key

--- bloom/files/src/entities.rs
+++ bloom/files/src/entities.rs
@@ -28,11 +28,12 @@ impl File {
     }
 
     pub fn storage_key(&self) -> String {
-        // TODO: is using folders really needed?
-        // for traditional FS -> yes
-        // for S3: seems not
+        // we split files into folder to not have too many files
+        // in the same folder, which may have a huge performance impact on traditional
+        // filesystems.
+        // Here we can have up to 2^12 = 4096 files per folder
         let id_str = self.id.to_hyphenated().to_string();
-        format!("/files/{}/{}", &id_str[..4], &id_str)
+        format!("/files/{}/{}", &id_str[..3], &id_str[3..])
     }
 }
 
@@ -45,7 +46,7 @@ mod tests {
     fn file_storage_key() {
         let id = "c2ae4298-48a2-478b-a9f2-5eef5d9b54cd".parse::<Uuid>().unwrap();
         let now = Utc::now();
-        let expected_storage_key = "/files/c2ae/c2ae4298-48a2-478b-a9f2-5eef5d9b54cd".to_string();
+        let expected_storage_key = "/files/c2a/e4298-48a2-478b-a9f2-5eef5d9b54cd".to_string();
 
         let file = File {
             id,
