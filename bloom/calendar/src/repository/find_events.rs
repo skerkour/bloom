42e6ad76fe41f73a2b95257438c821da628351cb
Sylvain Kerkour <sylvain@kerkour.fr>
2021-02-05 17:28:57 +0000

    bloom/calendar: implement repository.find_events

--- bloom/calendar/src/repository/find_events.rs
+++ bloom/calendar/src/repository/find_events.rs
@@ -16,6 +16,23 @@ impl Repository {
         from: DateTime<Utc>,
         to: DateTime<Utc>,
     ) -> Result<Vec<Event>, Error> {
-        todo!();
+        const QUERY: &str = "SELECT * FROM calendar_events
+            WHERE namespace_id = $1
+                AND (start_at BETWEEN $2 AND $3
+                    OR end_at BETWEEN $2 AND $3)";
+
+        match sqlx::query_as::<_, Event>(QUERY)
+            .bind(namespace_id)
+            .bind(from)
+            .bind(to)
+            .fetch_all(db)
+            .await
+        {
+            Err(err) => {
+                error!("calendar.find_events: Finding events: {}", &err);
+                Err(err.into())
+            }
+            Ok(files) => Ok(files),
+        }
     }
 }
