03d7a424fb55c5eb542abb08c7acf01db091cc77
Sylvain Kerkour <sylvain@kerkour.fr>
2021-02-06 11:25:48 +0000

    webapp/calendar

---
+++ webapp/src/ui/components/calendar/event_dialog.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-dialog
+    v-model="show"
+    max-width="800px"
+    scrollable
+  >
+    <v-card>
+
+    </v-card>
+  </v-dialog>
+</template>
+
+
+<script lang="ts">
+import { VueApp } from '@/app/vue';
+import { CalendarEvent } from '@/domain/calendar/model';
+import { PropType } from 'vue';
+
+export default VueApp.extend({
+  name: 'BCalendarEventDialog',
+  props: {
+    value: {
+      type: Boolean as PropType<boolean>,
+      required: true,
+    },
+    event: {
+      type: Object as PropType<CalendarEvent | null>,
+      required: false,
+      default: null,
+    },
+  },
+  computed: {
+    show: {
+      get(): boolean {
+        return this.value;
+      },
+      set(value: boolean) {
+        this.$emit('input', value);
+      },
+    },
+  },
+});
+</script>
+
+
+<style lang="scss" scoped>
+</style>
--- webapp/src/ui/views/calendar/calendar.vue
+++ webapp/src/ui/views/calendar/calendar.vue
@@ -11,13 +11,20 @@
 
         <!-- <v-spacer /> -->
 
-      <p class="ma-0 blm-pointer ml-3" @click="centerToday">
+      <p class="ma-0 blm-pointer ml-3 d-none d-sm-flex" @click="centerToday">
         {{ today }}
       </p>
 
+      <v-spacer />
+
+       <v-btn right absolute color="primary" @click="openEventDialog" depressed>
+        <v-icon left>mdi-plus</v-icon>
+        Create Event
+      </v-btn>
+
       </v-toolbar>
 
-    <div class="fill-height" style="height: calc(100vh - 56px)">
+    <div class="fill-height" style="height: calc(100vh - 113px)">
       <v-calendar
         ref="calendar"
         v-model="focus"
@@ -25,26 +32,65 @@
       />
     </div>
 
+    <b-calendar-event-dialog
+      v-model="showEventDialog"
+      :event="currentEvent"
+    />
+
   </v-container>
 </template>
 
 
 <script lang="ts">
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { VueApp } from '@/app/vue';
+import { CalendarEvent } from '@/domain/calendar/model';
 import moment from 'moment';
+import BCalendarEventDialog from '@/ui/components/calendar/event_dialog.vue';
 
 export default VueApp.extend({
   name: 'BCalendarView',
+  components: {
+    BCalendarEventDialog,
+  },
   data() {
     return {
       now: moment().format('YYYY-MM-DD'),
       focus: moment().format('YYYY-MM-DD'),
       today: moment().format('dddd ll'),
+      showEventDialog: false,
+      currentEvent: null as CalendarEvent | null,
+      events: [] as CalendarEvent[],
     };
+  },
+  computed: {
+    vuetifyEvents: {
+      get(): any[] {
+        return this.events.map((ev) => {
+          const event = ev as any;
+          event.start_at_date = moment(ev.start_at).toDate();
+          event.end_at_date = moment(ev.end_at).toDate();
+          event.start = this.formatDateForVuetify(event.start_at_date);
+          event.end = this.formatDateForVuetify(event.end_at_date);
+          event.name = ev.title || '(No title)';
+          return event;
+        });
+      },
+    },
   },
   methods: {
     centerToday() {
       this.focus = this.now;
+    },
+    openEventDialog() {
+      this.showEventDialog = true;
+    },
+    closeEventDialog() {
+      this.showEventDialog = false;
+      this.currentEvent = null;
+    },
+    formatDateForVuetify(date: Date) {
+      return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
     },
   },
 });
