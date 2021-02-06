fd94dee866f609167a6e857d3c2a7b7fa060a2bc
Sylvain Kerkour <sylvain@kerkour.fr>
2021-02-06 22:09:08 +0000

    bloom/kernel: implement service.admin_find_users and service.admin_find_groups

---
+++ bloom/kernel/src/repository/find_all_groups.rs
@@ -0,0 +1,18 @@
+use super::Repository;
+use crate::{db::Queryer, entities, errors::kernel::Error};
+use stdx::log::error;
+use stdx::sqlx;
+
+impl Repository {
+    pub async fn find_all_groups<'c, C: Queryer<'c>>(&self, db: C) -> Result<Vec<entities::Group>, Error> {
+        const QUERY: &str = "SELECT * FROM kernel_groups";
+
+        match sqlx::query_as::<_, entities::Group>(QUERY).fetch_all(db).await {
+            Err(err) => {
+                error!("kernel.find_all_groups: finding groups: {}", &err);
+                Err(err.into())
+            }
+            Ok(res) => Ok(res),
+        }
+    }
+}
