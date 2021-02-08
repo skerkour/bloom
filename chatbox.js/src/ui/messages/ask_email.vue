3b2c2451392c61664ced2a2f539c1c017251663a
Sylvain Kerkour <sylvain@kerkour.fr>
2021-02-08 22:54:18 +0000

    chatbox.js: ask for email

--- chatbox.js/src/ui/messages/ask_email.vue
+++ chatbox.js/src/ui/messages/ask_email.vue
@@ -3,16 +3,31 @@
     <div>
       What is your email address?
     </div>
-    <div>
-      <input v-model="email" class="email-input" v-on:keyup.enter="linkContact" />
+    <div v-if="error">
+      Error: {{ error }}
+    </div>
+    <div class="ask-email-input">
+      <input
+        v-model="email"
+        class="email-input"
+        type="email"
+      />
+      <span @click="linkContact" class="pointer send-btn">
+        <b-send-icon alt="send icon" color="white" />
+      </span>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { VueApp } from '@/app/vue';
+import BSendIcon from '@/assets/send.vue';
+
 
 export default VueApp.extend({
+  components: {
+    BSendIcon,
+  },
   name: 'AskEmailMessage',
   data() {
     return {
@@ -68,10 +83,24 @@ export default VueApp.extend({
   min-width: 1%;
 }
 
+.ask-email-input {
+  display: flex;
+  align-items: center;
+}
+
 .email-input {
   margin-top: 10px;
   border: 1px solid #e9e9e9;
   border-radius: 5px;
   padding: 0.4rem 0.8rem;
+}
+
+.pointer {
+  cursor: pointer;
+}
+
+.send-btn {
+  margin-left: 6px;
+  padding-top: 12px;
 }
 </style>
