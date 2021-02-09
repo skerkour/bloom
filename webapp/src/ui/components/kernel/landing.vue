2f4490e11364a6029a745d0a69f19567aa16b6da
Sylvain Kerkour <sylvain@kerkour.fr>
2021-02-09 17:35:36 +0000

    webapp/landing: I want to...

--- webapp/src/ui/components/kernel/landing.vue
+++ webapp/src/ui/components/kernel/landing.vue
@@ -16,14 +16,14 @@
     <v-row class="text-center" justify="center" align="center">
       <v-col cols="12" md="6">
         <h2 class="text-h3 mb-5 mt-5 pt-4">
-          All-in-one workspace
+          The all-in-one workspace
         </h2>
-        <h3 class="text-h4">
+        <!-- <h3 class="text-h4">
           Take back control of your productivity!
         </h3>
         <h4 class="text-h5">
           Bloom is the easiest way to de-Google your life and business.
-        </h4>
+        </h4> -->
 
         <v-row class="text-center text--darken-1 grey--text pt-4 mt-5"
           align="center" justify="center" >
@@ -68,6 +68,62 @@
       </v-col>
     </v-row>
 
+
+    <v-row class="my-5 py-5" justify="center">
+      <v-col cols="12" md="8" class="text-center">
+        <h2 class="text-h2 font-weight-medium">
+          I want to
+        </h2>
+      </v-col>
+
+      <v-col cols="12" md="10" lg="8" xl="6">
+        <v-row>
+          <v-col cols="12" md="6" class="text-center my-5">
+            <v-row justify="center">
+              <v-col cols="12">
+                <!-- <img src="/assets/imgs/landing/superhero.svg" /> -->
+                <v-img alt="Super Hero"
+                  src="/assets/imgs/landing/superhero.svg" height="200px" contain />
+              </v-col>
+              <v-col cols="12">
+                <h3 class="text-h4">
+                  De-Google my life
+                </h3>
+              </v-col>
+
+              <v-col cols="12">
+                <v-btn depressed color="success" to="/register" class="b-btn-lower">
+                  Bloom for Individuals
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-col>
+
+
+          <v-col cols="12" md="6" class="text-center my-5">
+            <v-row justify="center">
+              <v-col cols="12">
+                <v-img alt="Productivity"
+                  src="/assets/imgs/landing/productivity.svg" height="200px" contain />
+              </v-col>
+              <v-col cols="12">
+                <h3 class="text-h4">
+                  Boost my productivity
+                </h3>
+              </v-col>
+
+              <v-col cols="12">
+                <v-btn depressed color="primary" to="/register" class="b-btn-lower">
+                  Bloom for Businesses
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-col>
+
+    </v-row>
+
     <b-features class="b-features" />
 
 
@@ -95,6 +151,10 @@ export default VueApp.extend({
 
 <style lang="scss" scoped>
 .b-features {
-  margin-top: 30px;
+  margin-top: 80px;
+}
+
+.b-btn-lower {
+  text-transform: none !important;
 }
 </style>
