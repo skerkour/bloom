f987407b8223540e5cf3f85a722dbae6f14e76a8
Sylvain Kerkour <sylvain@kerkour.fr>
2021-03-04 15:33:36 +0000

    webapp: disable chatbox

--- webapp/public/assets/js/bloom.js
+++ webapp/public/assets/js/bloom.js
@@ -9,6 +9,7 @@
   window.$bloom = {
     project: '01776890-92c0-a614-3490-25d136ee2abe',
     analytics: analytics,
+    chatbox: false,
   };
 
   d=document;s=d.createElement("script");s.src="https://bloom.sh/libs/bloom.js"; s.async=1;d.getElementsByTagName("head")[0].appendChild(s);
